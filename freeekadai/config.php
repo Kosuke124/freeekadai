090e2bb1a1c196f59d53f45fbb5e76025a1071ce
Kosuke124 <125632248+Kosuke124@users.noreply.github.com>
2023-03-25 12:35:32 +0900

    Update config.php

--- freeekadai/config.php
+++ freeekadai/config.php
@@ -2,6 +2,6 @@
 
 //ini_set('display_errors', 1);
 
-define('DSN', 'mysql:host=mysql17.onamae.ne.jp;dbname=m0i1t_kintaikanri_db');
-define('DB_USER', 'm0i1t_kosuke124');
-define('DB_PASS', 'analonany2020!');
+define('DSN', 'mysql:host=host;dbname=dbname');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
